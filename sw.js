4e891987c0107d2fa241e02570646eb98ed3ce45
kanjimultiservices-crypto <kanjimultiservices@gmail.com>
2026-06-24 14:02:30 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,51 @@
+// KANJI MULTI SERVICES - Service Worker PWA
+const CACHE_NAME = 'kanji-v1';
+const urlsToCache = [
+  '/kanji-multi-services/',
+  '/kanji-multi-services/index.html'
+];
+
+// Installation
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activation
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch - Network first, fallback to cache
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function(response) {
+        if (response && response.status === 200) {
+          var responseClone = response.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      })
+      .catch(function() {
+        return caches.match(event.request);
+      })
+  );
+});
